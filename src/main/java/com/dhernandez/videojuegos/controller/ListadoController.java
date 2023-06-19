c9c2439cf8fa19c28759c00902744e3d266ad0da
Daniel Hernandez <daniher02@gmail.com>
2023-06-19 12:50:48 -0400

    first commit

---
+++ src/main/java/com/dhernandez/videojuegos/controller/ListadoController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dhernandez.videojuegos.controller;
+
+import com.dhernandez.videojuegos.domain.Videojuego;
+import com.dhernandez.videojuegos.service.VideojuegoService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author danih
+ */
+@Controller
+public class ListadoController {
+    
+    private final VideojuegoService videojuegoService;
+    
+    public ListadoController(VideojuegoService videojuegoService) {
+        this.videojuegoService = videojuegoService;
+    }
+    
+    @RequestMapping("/")
+    public String listarVideojuegos(Model model){
+        //busqueda de videojuegos
+        List<Videojuego> destacados = videojuegoService.buscarDestacatos();
+        model.addAttribute("videojuegos", destacados);
+        return("listado");
+    }
+}
---
+++ src/main/java/com/dhernandez/videojuegos/domain/Videojuego.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dhernandez.videojuegos.domain;
+
+/**
+ *
+ * @author danih
+ */
+public class Videojuego {
+
+    private String nombre;
+    private String descripcion;
+    private String imagenUrl;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+    
+    
+
+}
