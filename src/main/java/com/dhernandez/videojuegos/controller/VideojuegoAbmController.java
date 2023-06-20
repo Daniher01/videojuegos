d5c2837ed94bcde36e56c3f0484063939f23c372
Daniel Hernandez <daniher02@gmail.com>
2023-06-20 17:20:00 -0400

    se crea formulario para dar de alta un videojuego

---
+++ src/main/java/com/dhernandez/videojuegos/controller/VideojuegoAbmController.java
@@ -0,0 +1,13 @@
+package com.dhernandez.videojuegos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VideojuegoAbmController {
+
+    @RequestMapping("/videojuegos/crear")
+    public String mostrarFormAlta() {
+        return "formVideojuego";
+    }
+}
