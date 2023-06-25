8c0d04ff34cd04bef7e4bb8b2993b0f85bae9aad
Daniel Hernandez <daniher02@gmail.com>
2023-06-25 13:57:16 -0400

    se refactoriza y se crea api rest

--- src/main/java/com/dhernandez/videojuegos/api/controller/VideojuegoController.java
+++ src/main/java/com/dhernandez/videojuegos/api/controller/VideojuegoController.java
@@ -2,26 +2,39 @@ package com.dhernandez.videojuegos.api.controller;
 
 import com.dhernandez.videojuegos.domain.Videojuego;
 import com.dhernandez.videojuegos.service.VideojuegoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
-public class ListadoApiController {
+@RequestMapping("/api/videojuegos")
+public class VideojuegoController {
 
     private final VideojuegoService videojuegoService;
 
-    public ListadoApiController(VideojuegoService videojuegoService) {
+    public VideojuegoController(VideojuegoService videojuegoService) {
         this.videojuegoService = videojuegoService;
     }
 
 
-    @GetMapping("/videojuegos")
+    @GetMapping("")
     public List<Videojuego> listarVideojuegos(){
         List<Videojuego> destacados = videojuegoService.buscarDestacatos();
         return  destacados;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Videojuego> buscarVideojuegos(@PathVariable int id){
+        Optional<Videojuego> videojuego = videojuegoService.buscarPorId(id);
+        if(videojuego.isPresent()){
+            return ResponseEntity.ok(videojuego.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
